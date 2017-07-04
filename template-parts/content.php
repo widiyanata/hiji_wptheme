7facdee7570de9201de9e1e569c279d4a29f25a1
widiyanata <widyanata48@gmail.com>
2017-07-04 11:52:06 +0700

    Styling Posts List

--- template-parts/content.php
+++ template-parts/content.php
@@ -8,40 +8,47 @@
  */
 
 ?>
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<?php if ( has_post_thumbnail() ) {
+			the_post_thumbnail();
+		} else { ?>
+			<img src="http://lorempixel.com/300/300/" alt="<?php the_title(); ?>">
+		<?php } ?>
+		<div class="article-content">
+			<header class="entry-header">
+				<?php
+				if ( is_single() ) :
+					the_title( '<h1 class="entry-title">', '</h1>' );
+					else :
+						the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+					endif;
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
-		if ( is_single() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
+					if ( 'post' === get_post_type() ) : ?>
+					<div class="entry-meta">
+						<?php hiji_posted_on(); ?>
+					</div><!-- .entry-meta -->
+					<?php
+				endif; ?>
+			</header><!-- .entry-header -->
 
-		if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php hiji_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php
-		endif; ?>
-	</header><!-- .entry-header -->
+			<div class="entry-content text-justify">
+				<?php
+				the_content( sprintf(
+					/* translators: %s: Name of current post. */
+					wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'hiji' ), array( 'span' => array( 'class' => array() ) ) ),
+					the_title( '<span class="screen-reader-text">"', '"</span>', false )
+					) );
 
-	<div class="entry-content">
-		<?php
-			the_content( sprintf(
-				/* translators: %s: Name of current post. */
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'hiji' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
+					wp_link_pages( array(
+						'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'hiji' ),
+						'after'  => '</div>',
+						) );
+						?>
+					</div><!-- .entry-content -->
+			<footer class="entry-footer">
+						<?php hiji_entry_footer(); ?>
+					</footer><!-- .entry-footer -->
 
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'hiji' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
+		</div>
 
-	<footer class="entry-footer">
-		<?php hiji_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-## -->
+	</article><!-- #post-## -->
