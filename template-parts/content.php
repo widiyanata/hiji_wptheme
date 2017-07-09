4c3d4a17815d2cc96b894775f32e5996acd817f3
widiyanata <widyanata48@gmail.com>
2017-07-09 15:28:03 +0700

    Minor change to site header and blog post

--- template-parts/content.php
+++ template-parts/content.php
@@ -10,37 +10,50 @@
 ?>
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<div class="row">
-			<div class="col-md-4">
+			<div class="col-md-5">
 				<?php
 				if ( is_home() ){
 					if ( has_post_thumbnail()  ) {
 						the_post_thumbnail();
-					} else {
-						if ( !is_page() ) {?>
-							<img src="http://lorempixel.com/300/300/" alt="<?php the_title(); ?>">
+					}
+				} else {
+						if ( !is_page() ) {
+							if ( has_post_thumbnail()  ) {
+								the_post_thumbnail();
+							} else { ?>
+								<img src="http://lorempixel.com/300/300/" alt="<?php the_title(); ?>">
 							<?php }
+							 }
 						}
-					} ?>
+					 ?>
 			</div>
-			<div class="col-md-8">
-				<div class="article-content">
-					<header class="entry-header">
-						<?php
-						if ( is_single() ) :
-							the_title( '<h1 class="entry-title">', '</h1>' );
-							else :
-								the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-							endif;
+			<div class="col-md-7">
+				<header class="entry-header">
+					<?php
+					if ( is_single() ) :
+						the_title( '<h1 class="entry-title">', '</h1>' );
+						else :
+							the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+						endif;
 
-							if ( 'post' === get_post_type() ) : ?>
-							<div class="entry-meta">
-								<?php hiji_posted_on(); ?>
-							</div><!-- .entry-meta -->
+						if ( 'post' === get_post_type() ) : ?>
+						<div class="entry-meta">
 							<?php
-						endif; ?>
-					</header><!-- .entry-header -->
+							hiji_posted_on();
+							?>
+							<br>
+							<!-- <p>by: <?php the_author(); ?></p> -->
+							<p>in: <?php the_category(' | '); ?></p>
+						</div><!-- .entry-meta -->
+						<?php
+					endif; ?>
+				</header><!-- .entry-header -->
+			</div>
 
-					<div class="entry-content text-justify">
+			<div class="col-md-12">
+				<div class="article-content">
+
+					<div class="entry-content text-justify <?php echo ( !is_home()  ? '' : 'sr-only'); ?>">
 						<?php
 
 						if ( is_single() ) {
